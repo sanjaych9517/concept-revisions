630c5605c2131a87ef370776df79e66d7edb80c4
unknown <sanjaych9517@gmail.com>
2026-09-20 17:35:39 +0530

    counte app with theme change

--- Color and theme app/src/App.jsx
+++ Color and theme app/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const App = () => {
 
   const [count, setCount] = useState(0);
+  const [darkTheme, setDarkTheme] = useState(true);
 
 // increment function
 const increment = () =>{
@@ -19,14 +20,37 @@ const reset = () => {
 setCount(0)
 }
 
+const toggle = () =>{
+  setDarkTheme(!darkTheme)
+}
+
   return (
-    <div className="flex justify-center flex-col items-center bg-red-300 gap-3 ">
-      <h1 className="text-3xl">Counter App</h1>
-      <h2>count = {count}</h2>
+    <div
+      className={`min-h-screen flex flex-col items-center justify-center gap-6 ${darkTheme? "bg-gray-900 text-white" : "bg-gray-100 text-black"}`}
+    >
+<button onClick={toggle}  className="px-5 py-2 rounded bg-blue-600 text-white">{darkTheme? "☀️ Light Mode" : "🌙 Dark Mode"}</button>
+
+      <h1 className="text-4xl font-bold">Counter App</h1>
+      <h2 className="text-6xl">{count}</h2>
       <div className="flex gap-5  ">
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={reset}>Reset</button>
+        <button
+          onClick={increment}
+          className="bg-green-600 px-5 py-2 text-2xl text-white rounded"
+        >
+          Inc ++
+        </button>
+        <button
+          onClick={decrement}
+          className="bg-red-600 px-5 py-2 text-2xl text-white rounded"
+        >
+          Dec --
+        </button>
+        <button
+          onClick={reset}
+          className="bg-gray-600 px-5 text-2xl py-2 text-white rounded"
+        >
+          Reset
+        </button>
       </div>
     </div>
   );
